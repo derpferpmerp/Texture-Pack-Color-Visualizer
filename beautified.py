1b9d591d753a4c64c5a1e101fb28c50f76f7fe61
derpferpmerp <47375701+derpferpmerp@users.noreply.github.com>
2021-06-22 13:07:01 -0500

    Update beautified.py

--- beautified.py
+++ beautified.py
@@ -1,17 +1,27 @@
-from Color import GColor
-import sys, os
+import os
+import sys
 
-mcmetalist = list([
-    x for x in os.listdir("./MCMETA/")
-    if os.path.isfile(os.path.join("./MCMETA/", x))
-    and x.split(".")[-1] == "mcmeta"
-])
-lstdesc=list([[__import__("json").loads(line) for line in open(f"./MCMETA/{g}.mcmeta", 'r').read().split('\n')][0]["pack"]["description"] for g in list([os.path.splitext(os.path.basename(f"./MCMETA/{n}"))[0] for n in mcmetalist])])
-finallst = list([{
-    os.path.splitext(
-        os.path.basename(f"./MCMETA/{mcmetalist[lstdesc.index(k)]}"))[0]:
-    k.replace(u"\u00A7", "?")
-} for k in lstdesc])
+from Color import GColor
+
+
+mcmetalist = [
+    x
+    for x in os.listdir("./MCMETA/")
+    if os.path.isfile(os.path.join("./MCMETA/", x)) and x.split(".")[-1] == "mcmeta"
+]
+lstdesc = [[
+        __import__("json").loads(line)
+        for line in open(f"./MCMETA/{g}.mcmeta").read().split("\n")
+][0]["pack"]["description"]
+    for g in [os.path.splitext(os.path.basename(f"./MCMETA/{n}"))[0] for n in mcmetalist]]
+finallst = [
+    {
+        os.path.splitext(os.path.basename(f"./MCMETA/{mcmetalist[lstdesc.index(k)]}"))[
+            0
+        ]: k.replace("\u00A7", "?"),
+    }
+    for k in lstdesc
+]
 global possible
 possible = {
     "red": ["c", "[255, 85, 85]"],
@@ -29,58 +39,62 @@ possible = {
     "dark_gray": ["8", "[85, 85, 85]"],
     "dark_green": ["2", "[0, 170, 0]"],
     "dark_purple": ["5", "[170, 0, 170]"],
-    "light_purple": ["d", "[255, 85, 255]"]
+    "light_purple": ["d", "[255, 85, 255]"],
 }
-print(u"Denote color codes with ? instead of \u00A7")
+print("Denote color codes with ? instead of \u00A7")
 userin = str(input("Input: ")) if len(sys.argv) - 1 == 0 else sys.argv[1]
 print("\n")
 
 
 def prrgb(rgb, s):
-    return (GColor.RGB(rgb[0], rgb[1], rgb[2]) + s + GColor.END)
+    return GColor.RGB(rgb[0], rgb[1], rgb[2]) + s + GColor.END
 
 
 def rcolored(txt):
-    lstvals = list([cc for cc in possible.values()])
-    lstkeys = list([cc for cc in possible.keys()])
-    argb = list([
-        list(map(str.strip,
-                 g.strip('][').replace('"', '').split(',')))
-        for g in list(([x[1] for x in lstvals]))
-    ])
-    lstcodes = list([lstvals[v][0] for v in range(len(lstkeys))])
-    enumerated = list([str(txt[c]) for c in [i for i in range(len(txt))]])
+    lstvals = [cc for cc in possible.values()]
+    lstkeys = [cc for cc in possible.keys()]
+    argb = [
+        list(map(str.strip, g.strip("][").replace('"', "").split(",")))
+        for g in [x[1] for x in lstvals]
+    ]
+    lstcodes = [lstvals[v][0] for v in range(len(lstkeys))]
+    enumerated = [str(txt[c]) for c in [i for i in range(len(txt))]]
     infolst = {}
     finalstring = ""
     for x in range(len(enumerated) - 1):
         if enumerated[x] == "?" and enumerated[x + 1] in lstcodes:
             infolst[f"{x+2}"] = argb[lstcodes.index(enumerated[x + 1])]
-    lstkeysinfo = list([g for g in infolst.keys()])
-    lstvalsinfo = list([g for g in infolst.values()])
+    lstkeysinfo = [g for g in infolst.keys()]
+    lstvalsinfo = [g for g in infolst.values()]
     for x in range(len(lstkeysinfo)):
         if x != len(lstkeysinfo) - 1:
             finalstring += prrgb(
                 lstvalsinfo[x],
-                str("".join(enumerated[int(lstkeysinfo[int(x)]
-                                           ):int(lstkeysinfo[int(x) + 1]) -
-                                       2])))
+                str(
+                    "".join(
+                        enumerated[
+                            int(lstkeysinfo[int(x)]) : int(lstkeysinfo[int(x) + 1]) - 2
+                        ],
+                    ),
+                ),
+            )
         else:
             finalstring += prrgb(
-                lstvalsinfo[x],
-                "".join(enumerated[int(lstkeysinfo[int(x)]):len(txt)]))
+                lstvalsinfo[x], "".join(enumerated[int(lstkeysinfo[int(x)]) : len(txt)]),
+            )
     if finalstring != None:
         print(f"{finalstring}")
 
 
 rcolored(userin)
-lstodesc = list(
-    ["\n\t" + str(g).replace(u"\u00A7", "?") + "\n" for g in lstdesc])
-lstnames = list([
-    f"?fUsing?f ?b{list([k for k in finallst[o].keys()])[0]}?b?f.?f?amcmeta?a?f:?f\n{lstodesc[o]}"
+lstodesc = ["\n\t" + str(g).replace("\u00A7", "?") + "\n" for g in lstdesc]
+lstnames = [
+    f"?fUsing?f ?b{[k for k in finallst[o].keys()][0]}?b?f.?f?amcmeta?a?f:?f\n{lstodesc[o]}"
     for o in range(len(finallst))
-])
+]
 print("\n")
 [
-    f"{rcolored(lstnames[x])}" for x in range(len(lstnames))
+    f"{rcolored(lstnames[x])}"
+    for x in range(len(lstnames))
     if rcolored(lstnames[x]) != None
 ]
